2cd705c691f75d7fb35a0916784a889dd227c1c9
seancnfg <37197643+seancnfg@users.noreply.github.com>
2018-04-16 23:14:45 -0700

    Add Problem 15 Encode Quest

---
+++ Encode Quest.py
@@ -0,0 +1,41 @@
+"""
+TASK: Given a string and a code word, print out an encoded string following the alphabet of the code word.
+"""
+import string
+
+
+def encode(sent, code):
+    """
+    Encodes the given string following the position of the alphabets in the code word.
+
+    :param sent: Given sentence to code
+    :param code: The code word the encoding process follows
+    :return: A single encoded string
+    """
+    alpha = string.ascii_uppercase
+    new_sent = ''
+    code_index = 0
+    for j in sent:
+        if j == " ":
+            new_sent += " "
+            continue
+
+        alpha_shift = alpha.index(code[code_index % len(code)])
+        alpha_add = alpha.index(j)
+        enc = (alpha_shift + alpha_add) % 26
+        new_sent += alpha[enc]
+        code_index += 1
+
+    return new_sent
+
+
+if __name__ == '__main__':
+    n = int(input())
+    answers = []
+    for i in range(n):
+        s = input()  # Sentence
+        c = input()  # Code
+        answers.append(encode(s, c))
+
+    for i in answers:
+        print(i)
